67996a6cfa011b091f84c96afeb6802110381647
Sun Young Bae <tjsdud6677@gmail.com>
2022-11-24 01:04:07 +0900

    PasswordEdit.js ProfileEdit.js update

--- src/pages/PasswordEdit.js
+++ src/pages/PasswordEdit.js
@@ -1,14 +1,45 @@
 //관리자 비밀번호 변경 페이지
 
-
-import React from 'react';
+import axios from 'axios';
+import React,{useState ,useEffect} from 'react';
 import Box from '@mui/material/Box';
 import './css/PasswordEdit.css';
 
 const PasswordEdit = () => {
+
+    useEffect(()=>{
+        if(localStorage.getItem('isLogined')) { 
+        }else{
+            alert('로그인이 필요합니다.');
+            window.location.replace("/Login");
+        }
+    },[]);
+
+    const [newPassword, setNewPassword] = useState("");
+
+    const handlePasswordChange = (event) => {
+        setNewPassword(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:5000/manager/password/'+localStorage.getItem('isLogined'), {
+            password:newPassword
+        }).then((res)=>{
+          if(res.status===200){
+            alert('비밀번호 변경 성공');
+            localStorage.removeItem('isLogined');
+            window.location.replace("/Login");
+          }
+        }).catch((err)=>{
+          alert('비밀번호 변경 실패');
+          console.log(err);
+        })
+      }
+
     return (
     <div className="PasswordEditFormWrapper">
-        {/* <form onSubmit={handleSubmit}> */}
+        <form onSubmit={handleSubmit}>
         <div className="PasswordEditFormTitle">
             <h1>비밀번호 변경</h1>
         </div>
@@ -19,7 +50,7 @@ const PasswordEdit = () => {
                     <input id="oldPassword" type="password"></input>
 
                     <label htmlfor="newPassword">새 비밀번호</label>
-                    <input id="newPassword" type="password"></input>
+                    <input id="newPassword" type="password" onChange={handlePasswordChange}></input>
 
                     <label htmlfor="confirmPassword">새 비밀번호 확인</label>
                     <input id="confirmPassword" type="password"></input>
@@ -27,7 +58,7 @@ const PasswordEdit = () => {
                 <button className="newPasswordbutton">변경</button>
             </Box>
         </div>
-        {/* </form> */}
+        </form>
     </div>
     )
 }
--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,27 +1,172 @@
 //관리자 프로필 정보 변경 페이지
 
 
-import React from 'react';
+import axios from 'axios';
+import React,{useState ,useEffect} from 'react';
 import Box from '@mui/material/Box';
 import './css/ProfileEdit.css';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const ProfileEdit = () => {
+
+    useEffect(()=>{
+        if(localStorage.getItem('isLogined')) { 
+            axios.get('http://localhost:5000/manager/Info/'+localStorage.getItem('isLogined'))
+            .then((res)=>{
+                setManagerData(res.data);
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+        }else{
+            alert('로그인이 필요합니다.');
+            window.location.replace("/Login");
+        }
+    },[]);
+
+    const [expanded, setExpanded] = useState(false);
+    const [managerData, setManagerData] = useState([]);
+
+    // const [Managerid, setManagerId] = useState("");
+    // const [name, setName] = useState("");
+    // const [phoneNum, setPhoneNum] = useState("");
+    // const [email, setEmail] = useState("");
+
+    // const handleIdChange = (event) => {
+    //     setManagerId(event.target.value);
+    // }
+  
+    // const handleNameChange = (event) => {
+    //     setName(event.target.value);
+    // }
+  
+    // const handlePhoneNumChange = (event) => {
+    //     setPhoneNum(event.target.value);
+    // }
+  
+    // const handleEmailChange = (event) => {
+    //     setEmail(event.target.value);
+    // }
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     axios.post('http://localhost:5000/customer/register', {
+    //       RRN:RRN,
+    //       name:name,
+    //       address:address,
+    //       landline_phone:landline_phone,
+    //       phone_num:phone_num,
+    //       remark:remark,
+    //       manager_id:manager_id
+    //     }).then((res)=>{
+    //       if(res.status===200){
+    //         alert('회원 정보 등록 성공');
+    //       }
+    //     }).catch((err)=>{
+    //       alert('회원 정보 등록 실패');
+    //       console.log(err);
+    //     })
+    //   }
+
     return (
         <div className="ProfileEditFormWrapper">
-            {/* <form onSubmit={handleSubmit}> */}
                 <div className="ProfileEditFormTitle">
                     <h1>개인정보 관리</h1>
                 </div>
+                {/* <form onSubmit={handleSubmit}> */}
                 <div className="ProfileEditFormSection">
-
-                    <Box sx={{ height: 100, width: 900, border: 1, marginBottom: 5,}} >
+                    <Box sx={{ height: 220, width: 900, marginBottom: 5,}} >
                         <div className="ProfileBox">
                             {/* 관리자 프로필 ( 이메일, 이름 ) 들어가는 부분 */}
                             <label>관리자 프로필</label>
+                            <div>
+                                <Accordion sx={{marginTop:3}} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                                    <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1bh-content"
+                                    id="panel1bh-header"
+                                    >
+                                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                                        아이디
+                                    </Typography>
+                                    <Typography sx={{ color: 'text.secondary' }}>{managerData[0].id}</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                    <Typography>
+                                        Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+                                        Aliquam eget maximus est, id dignissim quam.
+                                    </Typography>
+                                    </AccordionDetails>
+                                </Accordion>
+                                <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                                    <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel2bh-content"
+                                    id="panel2bh-header"
+                                    >
+                                    <Typography sx={{ width: '33%', flexShrink: 0 }}>이름</Typography>
+                                    <Typography sx={{ color: 'text.secondary' }}>
+                                        {managerData[0].name}
+                                    </Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                    <Typography>
+                                        Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
+                                        varius pulvinar diam eros in elit. Pellentesque convallis laoreet
+                                        laoreet.
+                                    </Typography>
+                                    </AccordionDetails>
+                                </Accordion>
+                                <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                                    <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel3bh-content"
+                                    id="panel3bh-header"
+                                    >
+                                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                                        휴대폰 번호
+                                    </Typography>
+                                    <Typography sx={{ color: 'text.secondary' }}>
+                                        {managerData[0].phone_num}
+                                    </Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                    <Typography>
+                                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+                                        amet egestas eros, vitae egestas augue. Duis vel est augue.
+                                    </Typography>
+                                    </AccordionDetails>
+                                </Accordion>
+                                <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                                    <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel4bh-content"
+                                    id="panel4bh-header"
+                                    >
+                                    <Typography sx={{ width: '33%', flexShrink: 0 }}>이메일</Typography>
+                                    <Typography sx={{ color: 'text.secondary' }}>
+                                        {managerData[0].e_mail}
+                                    </Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                    <Typography>
+                                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+                                        amet egestas eros, vitae egestas augue. Duis vel est augue.
+                                    </Typography>
+                                    </AccordionDetails>
+                                </Accordion>
+                            </div>
                         </div>
                     </Box>
-
-                    <Box sx={{ height: 400, width: 900, border: 1 }} >
+                    <Box sx={{ marginTop: 0, height: 50, width: 900, border: 1, textAlign: 'center', paddingTop: 4}} >
                         <div className="MyInfoBox">
                             <label htmlfor="managerName">이름</label>
                             <input id="managerName" type="text"/>
